810d35cbf95ab2abbf9f4004852a9a306f027885
parkjungwoong34 <https://github.com/parkjungwoong34/test04.git>
2023-01-16 10:56:13 +0900

    day03 texts

---
+++ day03.py
@@ -0,0 +1,15 @@
+inha = 'a duck goes into a sea'
+print(inha.replace('a ', 'a nice'))
+pokemons_list = ['피카츄', '꼬부기', '이상해', '파이리']
+pokemons_string ='/'.join(pokemons_list)
+print(pokemons_string)
+
+univ = 'Inha University'
+print(univ.split())
+print(len(univ))
+print(univ[-10:-6])
+print(univ[5:-6])
+print(univ[::2])
+print(univ[5:])
+print(univ[5:15])
+print(univ[-10:])
