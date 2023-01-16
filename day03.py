ba52a30f05a9bef1f24bf0dbeaaa9b986dbdc2c2
parkjungwoong34 <https://github.com/parkjungwoong34/test04.git>
2023-01-16 11:46:02 +0900

    day03 first

--- day03.py
+++ day03.py
@@ -1,3 +1,13 @@
+subjects = '   python, data structure, database   $$$'
+print(subjects.title())
+print(subjects.count('data'))
+print(subjects.rfind('data'), subjects.index('data'))
+print(subjects.find('inha')), # -1 return
+print(subjects.index('inha')) # Exception !
+
+subjects = '   python, data structure, database   $$$'
+print(subjects.strip('$'))
+
 inha = 'a duck goes into a sea'
 print(inha.replace('a ', 'a nice'))
 pokemons_list = ['피카츄', '꼬부기', '이상해', '파이리']
@@ -13,3 +23,4 @@ print(univ[::2])
 print(univ[5:])
 print(univ[5:15])
 print(univ[-10:])
+
