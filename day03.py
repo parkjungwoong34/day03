0f4a5e477897c1558048971d921ac6ed8ac7b8a7
parkjungwoong34 <https://github.com/parkjungwoong34/test04.git>
2023-01-16 13:27:22 +0900

    excercise01to03

--- day03.py
+++ day03.py
@@ -1,26 +1,17 @@
-subjects = '   python, data structure, database   $$$'
-print(subjects.title())
-print(subjects.count('data'))
-print(subjects.rfind('data'), subjects.index('data'))
-print(subjects.find('inha')), # -1 return
-print(subjects.index('inha')) # Exception !
+print("My kitty cat likes %s\nMy kitty cat likes %s\nMy kitty cat likes %s")
 
-subjects = '   python, data structure, database   $$$'
-print(subjects.strip('$'))
+song = """When an eel grabs your arm,
+And it causes great harm,
+That's - a moray!"""
 
-inha = 'a duck goes into a sea'
-print(inha.replace('a ', 'a nice'))
-pokemons_list = ['피카츄', '꼬부기', '이상해', '파이리']
-pokemons_string ='/'.join(pokemons_list)
-print(pokemons_string)
+song_list = song.split()
+print(song_list)
+song_list[14] = song_list[14].title()
+song_string = ' '.join(song_list)
+print(song_list)
 
-univ = 'Inha University'
-print(univ.split())
-print(len(univ))
-print(univ[-10:-6])
-print(univ[5:-6])
-print(univ[::2])
-print(univ[5:])
-print(univ[5:15])
-print(univ[-10:])
+
+idx = song.rfind('m')
+song = song.replace(song[idx], song[idx].upper())
+print(song.endswith('moray!'))
 
