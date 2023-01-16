0abc6ace7a61b4a910e5b5dcf9e22843487e98a7
parkjungwoong34 <https://github.com/parkjungwoong34/test04.git>
2023-01-16 14:09:28 +0900

    day03while

--- day03.py
+++ day03.py
@@ -1,17 +1,27 @@
-print("My kitty cat likes %s\nMy kitty cat likes %s\nMy kitty cat likes %s")
+# while
 
-song = """When an eel grabs your arm,
-And it causes great harm,
-That's - a moray!"""
+numbers = [1, 3, 10]
+position = 0
+while position < len(numbers):
+    number = numbers[position]
+    if number % 2 == 0:
+        print('Found even number', number)
+        break
+    position += 1
+else: # break 문이 호출되지 않은 경우
+    print('No even number found')
 
-song_list = song.split()
-print(song_list)
-song_list[14] = song_list[14].title()
-song_string = ' '.join(song_list)
-print(song_list)
 
 
-idx = song.rfind('m')
-song = song.replace(song[idx], song[idx].upper())
-print(song.endswith('moray!'))
-
+# while True:
+#     dan = int(input('Dan (0 to quit): '))
+#
+#     if dan == 0: # exit
+#         break
+#     if 1 < dan < 10: #  if dan >= 2 and dan <= 9:
+#         i = 1
+#         while i < 10:
+#             print('{0} * {1} = {2}'.format(dan, i, dan * i))
+#             i = i + 1
+#     else:
+#         print('2에서 9사이의 값을 입력하세요')
