0fac0c35d0c8c4cb060b7838ddb83771a1f23352
parkjungwoong34 <https://github.com/parkjungwoong34/test04.git>
2023-01-16 17:56:58 +0900

    excercise6_1

--- day003.py
+++ day003.py
@@ -1,2 +1,13 @@
-letter = 'Dear {} {},\nThank you for letter. We are sorry that our {} {} in your \n{}.Please note that it should never be used in a {}, especially near any \n{}.\nSend us your receipt and {} for shipping and handling. We will send you \n another {} that, in our tests, is {}% less likely to have {}.\n Thank you for your support.\nSincerely,\n{}\n{}'.format('salutation', 'name', 'product', 'verbed', 'room', 'room', 'animals', 'amount', 'product', 'percent', 'verbed', 'spokesman', 'job_title')
-print(letter)
+guess_me = 7
+number = 1
+
+while True:
+    if guess_me > number:
+        print('too low')
+        break
+    elif guess_me < number:
+        print('oops')
+        break
+    else:
+        print('found out')
+        break
